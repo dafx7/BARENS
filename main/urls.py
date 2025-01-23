3cfe943951f40923c2512989a7785d05e00baa53
Dapnek <ahmaddaffaamd@gmail.com>
2025-01-24 02:27:17 +0700

    Improve room types page styles and functionality
    - Enhance layout and responsive design for dropdown filters and room listing.
    - Add image carousel with navigation inside modal for better user experience.
    - Update styles for buttons, dropdowns, and sections for a more polished appearance.
    - Implement JavaScript for modal image carousel functionality.

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),  # URL untuk halaman beranda
+    path('', views.index, name='index'),
+    path('tipe-kamar/', views.tipe_kamar, name='tipe_kamar'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
     return render(request, 'main/index.html')
+
+
+def tipe_kamar(request):
+    return render(request, 'main/tipe-kamar.html')
+
