0b984a12d4cf5e74711dea54b5f5c94e279222a6
Dapnek <ahmaddaffaamd@gmail.com>
2025-03-08 23:03:34 +0700

    Latest Loaddata

--- barens/settings.py
+++ barens/settings.py
@@ -91,22 +91,22 @@ WSGI_APPLICATION = 'barens.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'penyembahkalengb$barens',
-        'USER': 'penyembahkalengb',
-        'PASSWORD': 'daffa210978',
-        'HOST': 'penyembahkalengbiskuit.mysql.pythonanywhere-services.com',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'penyembahkalengb$barens',
+#         'USER': 'penyembahkalengb',
+#         'PASSWORD': 'daffa210978',
+#         'HOST': 'penyembahkalengbiskuit.mysql.pythonanywhere-services.com',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
